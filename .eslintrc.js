3622b23fba944ffa72b0679a191d1480a78ec92e
JossPerezT <jossperez44@gmail.com>
2022-05-03 15:44:12 -0500

    Modificando las reglas de eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
     "rules": {
         indent: ["error", 4], 
         "linebreak-style": ["error", "unix"],
-        quotes: ["error", "double"], 
         semi: ["error", "always"]
     }
 };
