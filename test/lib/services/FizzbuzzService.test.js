5b8e1a2f8ad1130ad0a906d30ef1c7be4de97b7b
JossPerezT <jossperez44@gmail.com>
2022-05-02 23:57:48 -0500

    26. Agregando la prueba 1 de FizzBuzz: Validación de Fizz

---
+++ test/lib/services/FizzbuzzService.test.js
@@ -0,0 +1,26 @@
+const FizzbuzzService = require('../../../lib/services/FizzbuzzService')
+
+describe ('Tests de FizzBuzz', ()=>{
+    test('1. Validación de Fizz', ()=>{
+        const explorer3 = {name: "Explorer3", score: 3}
+        FizzbuzzService.applyValidationInExplorer(explorer3)
+        expect(explorer3).toMatchObject({name: "Explorer3", score: 3, trick: "Fizz"})
+    })
+})
+
+
+
+/*
+const explorer1 = {name: "Explorer1", score: 1}
+FizzbuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+FizzbuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+FizzbuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+FizzbuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+*/
+
