04dec9f727f27fdd35e403c9f0f7fff7a2b098f7
JossPerezT <jossperez44@gmail.com>
2022-05-02 22:49:18 -0500

    20. Aplicando un match diferente para la prueba de Filtrar por mission: .toMatchObject

--- test/lib/services/ExplorerService.test.js
+++ test/lib/services/ExplorerService.test.js
@@ -9,7 +9,80 @@ const explorers = Reader.readJsonFile('./explorers.json')
 describe ('Tests de Explorer Service', ()=>{
     test('1. Filtar por mission', ()=> {
         const filtrarPorMission = ExplorerService.filterByMission(explorers, "node")
-        expect(filtrarPorMission[0].score).toBe(1)
+        expect(filtrarPorMission).toMatchObject(
+            [
+                {
+                  name: 'Woopa1',
+                  githubUsername: 'ajolonauta1',
+                  score: 1,
+                  mission: 'node',
+                  stacks: [ 'javascript', 'reasonML', 'elm' ]
+                },
+                {
+                  name: 'Woopa2',
+                  githubUsername: 'ajolonauta2',
+                  score: 2,
+                  mission: 'node',
+                  stacks: [ 'javascript', 'groovy', 'elm' ]
+                },
+                {
+                  name: 'Woopa3',
+                  githubUsername: 'ajolonauta3',
+                  score: 3,
+                  mission: 'node',
+                  stacks: [ 'elixir', 'groovy', 'reasonML' ]
+                },
+                {
+                  name: 'Woopa4',
+                  githubUsername: 'ajolonauta4',
+                  mission: 'node',
+                  score: 4,
+                  stacks: [ 'javascript' ]
+                },
+                {
+                  name: 'Woopa5',
+                  githubUsername: 'ajolonauta5',
+                  score: 5,
+                  mission: 'node',
+                  stacks: [ 'javascript', 'elixir', 'elm' ]
+                },
+                {
+                  name: 'Woopa11',
+                  githubUsername: 'ajolonauta11',
+                  score: 11,
+                  mission: 'node',
+                  stacks: [ 'javascript', 'elixir', 'groovy', 'reasonML', 'elm' ]
+                },
+                {
+                  name: 'Woopa12',
+                  githubUsername: 'ajolonauta12',
+                  score: 12,
+                  mission: 'node',
+                  stacks: [ 'javascript', 'elixir', 'groovy', 'reasonML', 'elm' ]
+                },
+                {
+                  name: 'Woopa13',
+                  githubUsername: 'ajolonauta13',
+                  score: 13,
+                  mission: 'node',
+                  stacks: [ 'javascript', 'elixir', 'groovy', 'reasonML', 'elm' ]
+                },
+                {
+                  name: 'Woopa14',
+                  githubUsername: 'ajolonauta14',
+                  score: 14,
+                  mission: 'node',
+                  stacks: [ 'javascript', 'elixir', 'groovy', 'reasonML', 'elm' ]
+                },
+                {
+                  name: 'Woopa15',
+                  githubUsername: 'ajolonauta15',
+                  score: 15,
+                  mission: 'node',
+                  stacks: [ 'javascript', 'elixir', 'groovy', 'reasonML', 'elm' ]
+                }
+            ]
+        )
         expect(filtrarPorMission[5].githubUsername).toBe("ajolonauta11")
     })
     test('2. Conseguir cuantos explorers hay por mission', ()=>{
@@ -18,5 +91,6 @@ describe ('Tests de Explorer Service', ()=>{
         const amountExplorerJava = ExplorerService.getAmountOfExplorersByMission(explorers, "java")
         expect (amountExplorerJava).toBe(5)
     })
+
 
 })
