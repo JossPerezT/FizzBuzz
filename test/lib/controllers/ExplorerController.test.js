a21435d961dfb77db9a20f84b701798abcfcd3f4
JossPerezT <jossperez44@gmail.com>
2022-05-03 16:00:30 -0500

    41. Corrigiendo la prueba 1 de ExplorerController

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -5,7 +5,7 @@ const ExplorerController = require('../../../lib/controllers/ExplorerController'
 
 describe("Tests para controlar la funcionalidad con el server", ()=>{
     test("1. Probando ExplorerController: GetExplorerByMission", ()=>{
-        const prueba1 = ExplorerController.getExplorersByMsission("java");
+        const prueba1 = ExplorerController.getExplorersByMsission("node");
         expect(prueba1).toMatchObject(
             [
                 {
