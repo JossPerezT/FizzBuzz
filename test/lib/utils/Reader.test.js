ed088f2157f6a93a54a40e0225675c7fa38b4d15
JossPerezT <jossperez44@gmail.com>
2022-05-02 20:59:06 -0500

    11. Testeando la prueba Reader con el archivo json correspondiente

--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -2,7 +2,7 @@ const Reader = require('../../../lib/utils/Reader')
 
 describe('Leer un archivo JSON', ()=>{
     test('1. Leer el archivo JSON', ()=>{
-        const explorers = Reader.readJsonFile('./test/lib/utils/ejemplo.json')
-        expect(explorers.name).toContain("Woopa1")
+        const explorers = Reader.readJsonFile('./explorers.json')
+        expect(explorers[0].name).toContain("Woopa1")
     })
 })
