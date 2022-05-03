84f227e4319d69ffb42cc42cfdb3f316e186658d
JossPerezT <jossperez44@gmail.com>
2022-05-02 21:51:22 -0500

    15. Agregando código para la prueba de filtrar por mission

---
+++ test/lib/services/ExplorerService.test.js
@@ -0,0 +1,15 @@
+
+
+const ExplorerService = require('../../../lib/services/ExplorerService')
+const Reader = require('../../../lib/utils/Reader')
+
+const explorers = Reader.readJsonFile('./explorers.json')
+
+
+describe ('Tests de Explorer Service', ()=>{
+    test('1. Filtar por mission', ()=> {
+        const filtrarPorMission = ExplorerService.filterByMission(explorers, "node")
+        expect(filtrarPorMission[0].score).toBe(1)
+    })
+
+})
