26f7fd94724c982de2f298f182159bb9594297bb
JossPerezT <jossperez44@gmail.com>
2022-05-03 21:29:26 -0500

    58. Corrigiendo la prueba 5 de fizzBuzz para todos los explorers

--- test/lib/controllers/ExplorerController.test.js
+++ test/lib/controllers/ExplorerController.test.js
@@ -194,7 +194,7 @@ describe("Tests para controlar la funcionalidad con el server", ()=>{
         expect(prueba5).toMatchObject(   
             [
                 {
-                    name: 'Woopa2',
+                    name: 'Woopa1',
                     githubUsername: 'ajolonauta1',
                     score: 1,
                     mission: 'node',
